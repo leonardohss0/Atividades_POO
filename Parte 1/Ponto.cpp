fd06d2652c27871e33a0719ed02ab6fa3b5ec297
leonardosanches <leo_urania01@hotmail.com>
2021-01-25 13:40:05 -0300

    ADD: Parte 1

---
+++ Parte 1/Ponto.cpp
@@ -0,0 +1,31 @@
+#include "Ponto.hpp"
+
+Ponto &Ponto::operator--()
+{
+    Ponto temp = *this;
+
+    this->x = --x;
+    this->y = --y;
+
+    return *this;
+}
+
+Ponto Ponto::operator+(const Ponto &P) const
+{
+    Ponto temp = *this;
+
+    temp.x = this->x + P.x;
+    temp.y = this->y + P.y;
+
+    return temp;
+}
+
+int main()
+{
+
+    Ponto a(1, 4), b(3, 2), c;
+    c = a + b; // soma as coordenadas dos pontos a e b
+    --c;       // decrementa em uma unidade as coordenadas de c
+
+    c.print();
+}
---
+++ Parte 1/Ponto.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class Ponto
+{
+
+    int x, y;
+
+public:
+    Ponto(int xx = 0, int yy = 0) : x(xx), y(yy){};
+    Ponto &operator--();
+    Ponto operator+(const Ponto &P) const;
+    ~Ponto(){};
+
+    inline int getX() { return this->x; }
+    inline int getY() { return this->y; }
+
+    inline void print() { std::cout << this->x << " " << this->y << std::endl; };
+};
