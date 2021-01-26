90143bb9e8ff4b5804572bc7adb3e6f754dac7fe
Luiz Almeida <luizorst@hotmail.com>
2021-01-26 18:54:20 -0300

    major refactor matrix class

--- atv_5/Parte 2/Matrix.cpp
+++ atv_5/Parte 2/Matrix.cpp
@@ -1,259 +1,293 @@
 #include "Matrix.hpp"
 #include <fstream>
 #include <iostream>
+#include <istream>
+
 
 Matrix::Matrix(int rows, int cols, const double &value) {
-  nRows = rows;
-  nCols = cols;
+    this->nRows = rows;
+    this->nCols = cols;
 
-  m = new double *[nRows];
-  for (int i = 0; i < nRows; i++) {
-    m[i] = new double[nCols];
-    for (int j = 0; j < nCols; j++) {
-      m[i][j] = value;
+    this->m = new double *[rows];
+
+    for(int i=0; i < this->nRows; ++i) {
+        m[i] = new double[cols];
+        for(int j=0; j < this->nCols; ++j) this->m[i][j] = value;
     }
-  }
 }
 
 Matrix::Matrix(std::ifstream &myFile) {
-  int fRows = 0, fCols = 0;
-  myFile >> fRows >> fCols;
+    int fRows = 0, fCols = 0;
+    myFile >> fRows >> fCols;
+    
+    this->nRows = fRows;
+    this->nCols = fCols;
+    this->m = new double *[fRows];
 
-  this->nRows = fRows;
-  this->nCols = fCols;
-  this->m = new double *[fRows];
-
-  for (int i = 0; i < fRows; ++i) {
-    this->m[i] = new double[fCols];
-    for (int j = 0; j < fCols; ++j) {
-      myFile >> this->m[i][j];
+    for (int i = 0; i < fRows; ++i){
+        this->m[i] = new double[fCols];
+        for (int j = 0; j < fCols; ++j) {
+            myFile >> this->m[i][j];
+        }       
     }
-  }
 }
 
-Matrix::Matrix(const Matrix &M) {
-  this->nRows = M.nRows;
-  this->nCols = M.nCols;
+Matrix::Matrix(const Matrix& that) {
+    this->nRows = that.getRows();
+    this->nCols = that.getCols();
 
-  m = new double *[nRows];
-  for (int i = 0; i < nRows; i++) {
-    m[i] = new double[nCols];
-    for (int j = 0; j < nCols; j++) {
-      this->m[i][j] = M.m[i][j];
+    this->m = new double *[this->nRows];
+
+    for (int i = 0; i < that.getRows(); i++){
+        this->m[i] = new double [this->nCols];
+        for (int j = 0; j < that.getCols(); j++)
+            this->m[i][j] = that.m[i][j];
     }
-  }
 }
 
 Matrix::~Matrix() {
-  for (int i = 0; i < nRows; i++) {
-    delete[] m[i];
-  }
-  delete[] m;
-}
-
-void Matrix::print() const {
-  for (int i = 0; i < nRows; i++) {
-    for (int j = 0; j < nCols; j++) {
-      std::cout << m[i][j] << " ";
+    if (this->getRows() >= 0 && this->getCols() >= 0) {
+        for (int i = 0; i < this->getRows(); ++i) 
+            delete [] this->m[i];
+        delete [] this->m;
     }
-    std::cout << std::endl;
-  }
-  std::cout << std::endl;
 }
 
-Matrix Matrix::operator*(const Matrix &M) const {
-  Matrix resultado(this->nRows, M.getCols());
-  double aux = 0;
-
-  for (int i = 0; i < this->nRows; i++) {
-    for (int j = 0; j < M.getCols(); j++) {
-      for (int n = 0; n < this->nCols; n++) {
-        aux = this->m[i][n] * M.m[n][j];
-        resultado.m[i][j] += aux;
-      }
-      aux = 0;
+// Permite ao usuario usar 1 based indexing.
+double Matrix::get(int row, int col) const {
+    if (this->getRows() >= row && row > 0){
+        if (this->getCols() >= col && col > 0)
+            return m[row - 1][col - 1];
     }
-  }
-  return resultado;
-}
-
-Matrix Matrix::operator=(const Matrix &M) {
-  for (int i = 0; i < nRows; i++) {
-    delete[] m[i];
-  }
-  delete[] m;
-
-  this->nRows = M.nRows;
-  this->nCols = M.nCols;
-
-  this->m = new double *[nRows];
-  for (int i = 0; i < this->nRows; i++) {
-    this->m[i] = new double[nCols];
-    for (int j = 0; j < this->nCols; j++) {
-      this->m[i][j] = M.m[i][j];
-    }
-  }
-  return *this;
-}
-
-Matrix &Matrix::operator+(const Matrix &M) const {
-  Matrix *sum = new Matrix(M.nRows, M.nCols);
-  for (int i = 0; i < nRows; i++) {
-    for (int j = 0; j < nCols; j++) {
-      sum->m[i][j] = this->m[i][j] + M.m[i][j];
-    }
-  }
-
-  return *sum;
-}
-
-bool Matrix::operator==(const Matrix &M) const {
-  if (this->nCols != M.nCols)
-    return false;
-  if (this->nRows != M.nRows)
-    return false;
-
-  int counter = 0;
-  for (int i = 0; i < this->nRows; i++) {
-    for (int j = 0; j < this->nCols; j++) {
-      if (this->m[i][j] == M.m[i][j]) {
-        counter++;
-      }
-    }
-  }
-
-  if (counter == nRows * nCols) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-bool Matrix::operator!=(const Matrix &M) const {
-  if (this->nCols != M.nCols)
-    return true;
-  if (this->nRows != M.nRows)
-    return true;
-
-  int counter = 0;
-  for (int i = 0; i < this->nRows; i++) {
-    for (int j = 0; j < this->nCols; j++) {
-      if (this->m[i][j] == M.m[i][j]) {
-        counter++;
-      }
-    }
-  }
-
-  if (counter == nRows * nCols) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-Matrix &Matrix::operator*=(const Matrix &M) {
-  Matrix *sub = new Matrix(this->nRows, M.nCols);
-
-  double sum;
-  for (int i = 0; i < nRows; i++) {
-    for (int j = 0; j < M.nCols; j++) {
-      sum = 0;
-      for (int k = 0; k < nCols; k++) {
-        sum += this->m[i][k] * M.m[k][j];
-      }
-      sub->m[i][j] = sum;
-    }
-  }
-
-  *this = *sub;
-
-  return *this;
-}
-
-Matrix &Matrix::operator*=(int value) {
-  for (int i = 0; i < nRows; i++) {
-    for (int j = 0; j < nCols; j++) {
-      this->m[i][j] *= value;
-    }
-  }
-  return *this;
-}
-
-Matrix &Matrix::operator+=(const Matrix &M) {
-  for (int i = 0; i < nRows; i++) {
-    for (int j = 0; j < nCols; j++) {
-      this->m[i][j] += M.m[i][j];
-    }
-  }
-
-  return *this;
-}
-
-Matrix &Matrix::operator-(const Matrix &M) const {
-  Matrix *sub = new Matrix(M.nRows, M.nCols);
-  sub->m = new double *[nRows];
-  for (int i = 0; i < nRows; i++) {
-    sub->m[i] = new double[nCols];
-    for (int j = 0; j < nCols; j++) {
-      sub->m[i][j] = this->m[i][j] - M.m[i][j];
-    }
-  }
-
-  return *sub;
-}
-
-Matrix &Matrix::operator-=(const Matrix &M) {
-  for (int i = 0; i < nRows; i++) {
-    for (int j = 0; j < nCols; j++) {
-      this->m[i][j] -= M.m[i][j];
-    }
-  }
-
-  return *this;
-}
-
-Matrix &Matrix::operator~() const {
-  Matrix *transp = new Matrix(nCols, nRows);
-
-  transp->m = new double *[transp->nRows];
-  for (int i = 0; i < transp->nCols; i++) {
-    transp->m[i] = new double[transp->nCols];
-    for (int j = 0; j < transp->nRows; j++) {
-      transp->m[i][j] = this->m[j][i];
-    }
-  }
-
-  return *transp;
-}
-
-double &Matrix::operator()(int i, int j) const { return this->m[i][j]; }
+    return -1;
+} 
 
 double Matrix::getZeroBased(int row, int col) const {
-  return this->m[row][col];
-}
-
-void Matrix::setValue(int row, int column, double value) {
-  this->m[row][column] = value;
-}
-
-std::ostream &operator<<(std::ostream &output, const Matrix &A) {
-  for (int i = 0; i < A.getRows(); ++i) {
-    for (int j = 0; j < A.getCols(); ++j) {
-      output << A.getZeroBased(i, j) << " ";
+    if (this->getRows() > row && row >= 0){
+        if (this->getCols() > col && col >= 0)
+            return m[row][col];
     }
-    output << std::endl;
-  }
-  return output;
+    return -1;
+} 
+
+void Matrix::setZeroBased(int row, int col, double value) {
+    if (this->getRows() > row && row >= 0){
+        if (this->getCols() > col && col >= 0)
+            this->m[row][col] = value;
+    } else std::cout << "Invalid position" << std::endl;
+} 
+
+void Matrix::print() const {
+    if (this->getCols() > 0 && this->getRows() > 0) {
+        for (int i = 0; i < this->getRows(); ++i) {
+            for (int j = 0; j < this->getCols(); ++j) {
+                std::cout << this->m[i][j] << ' ';
+            }
+            std::cout << std::endl;
+        }
+    }
 }
 
-std::istream &operator>>(std::istream &input, Matrix &A) {
-  double value = 0;
-
-  for (int i = 0; i < A.getRows(); ++i) {
-    for (int j = 0; j < A.getCols(); ++j) {
-      input >> value;
-      A.setValue(i, j, value);
+// Faz com que a matriz torne-se uma matriz identidade
+void Matrix::unit() {
+    if (this->getRows() == this->getCols()){
+        for (int i = 0; i < this->getRows(); i++){
+            for (int j = 0;j < this->getCols(); j++){
+                if (i==j)
+                    m[i][j]=1;
+                else
+                    m[i][j]=0;
+            }
+        }
     }
-  }
-  return input;
+}
+
+// Faz com que a matriz torne-se uma matriz nula
+void Matrix::zeros() {
+    for (int i = 0; i < this->getRows(); ++i){
+        for (int j = 0; j < this->getCols(); ++j)
+            m[i][j] = 0;
+    }
+}
+
+void Matrix::ones() {
+    for (int i = 0; i < this->getRows(); ++i){
+        for (int j = 0; j < this->getCols(); ++j)
+            m[i][j] = 1;
+    }
+    
+}
+
+double& Matrix::operator() (int i, int j) {
+    return this->m[i][j];
+}
+
+void Matrix::operator= (const Matrix &A) {
+    if (*(this) == A) return;
+    for (int i = 0; i < this->getRows(); ++i) {
+        delete [] this->m[i];
+    }
+    delete [] this->m;
+    
+    this->nCols = A.getCols(); this->nRows = A.getRows();
+    this->m = new double* [A.getRows()];
+
+    for (int i = 0; i < A.getRows(); ++i) {
+        this->m[i] = new double[this->getCols()];
+        for (int j = 0; j < A.getCols(); ++j) {
+            this->m[i][j] = A.getZeroBased(i, j);
+        }
+    }
+}
+
+bool Matrix::operator== (const Matrix& A) const {
+    if (this->getRows() != A.getRows() ||
+        this->getCols() != A.getCols()) return false;
+    for (int i = 0; i < this->getRows(); ++i) {
+        for (int j = 0; j < this->getCols(); ++j) {
+            if (this->getZeroBased(i, j) != A.getZeroBased(i, j))
+                return false;
+        }
+    }
+    return true;
+}
+
+bool Matrix::operator!= (const Matrix& A) const {
+    if (this->getRows() != A.getRows() ||
+        this->getCols() != A.getCols()) return true;
+    for (int i = 0; i < this->getRows(); ++i) {
+        for (int j = 0; j < this->getCols(); ++j) {
+            if (this->getZeroBased(i, j) != A.getZeroBased(i, j))
+                return true;
+        }
+    }
+    return false;
+}
+
+Matrix Matrix::operator+ (const Matrix &A) const {
+    if (A.getCols() == this->getCols() &&
+        A.getRows() == this->getRows()) {
+            Matrix sumMatrix(A.getCols(), A.getRows()); 
+            for (int i = 0; i < sumMatrix.getRows(); ++i) {
+                for (int j = 0; j < sumMatrix.getCols(); ++j) {
+                    sumMatrix.m[i][j] = A.getZeroBased(i, j) + this->getZeroBased(i, j);
+                }
+            }
+            return sumMatrix;
+        }
+    std::cout << "Not allowed to sum matrix of different dimensions" << std::endl;
+    return *this;
+}
+
+Matrix Matrix::operator- (const Matrix &A) const {
+    if (A.getCols() == this->getCols() &&
+        A.getRows() == this->getRows()) {
+            Matrix diffMatrix(A.getCols(), A.getRows()); 
+            for (int i = 0; i < diffMatrix.getRows(); ++i) {
+                for (int j = 0; j < diffMatrix.getCols(); ++j) {
+                    diffMatrix.m[i][j] = this->getZeroBased(i, j) - A.getZeroBased(i, j);
+                }
+            }
+            return diffMatrix;
+        }
+    std::cout << "Not allowed to sum matrix of different dimensions" << std::endl;
+    return *this;
+}
+
+Matrix Matrix::operator* (const Matrix &A) const {
+    if (A.getRows() == this->getCols()) {
+            Matrix multMatrix(this->getRows(), A.getCols()); 
+            for (int i = 0; i < this->getRows(); ++i) {
+                for (int j = 0; j < A.getCols(); ++j) {
+                    for (int k = 0; k < this->getCols(); ++k) {
+                        multMatrix.m[i][j] += this->getZeroBased(i, k) * A.getZeroBased(k, j);
+                    }
+                }
+            }
+        return multMatrix;
+    }
+    std::cout << "Bad dimensions for Matrix multiplication" << std::endl;
+    return *this;
+}
+
+void Matrix::operator-= (const Matrix &A) {
+    if (A.getCols() == this->getCols() &&
+        A.getRows() == this->getRows()) {
+            for (int i = 0; i < A.getRows(); ++i) {
+                for (int j = 0; j < A.getCols(); ++j) {
+                    this->m[i][j] -= A.getZeroBased(i, j);
+                }
+            }
+    } else {
+        std::cout << "Not allowed to subtrack matrix of different dimensions" << std::endl;
+    }
+}
+
+void Matrix::operator+= (const Matrix &A) {
+    if (A.getCols() == this->getCols() &&
+        A.getRows() == this->getRows()) {
+            for (int i = 0; i < A.getRows(); ++i) {
+                for (int j = 0; j < A.getCols(); ++j) {
+                    this->m[i][j] += A.getZeroBased(i, j);
+                }
+            }
+    } else {
+        std::cout << "Not allowed to sum matrix of different dimensions" << std::endl;
+    }
+}
+
+void Matrix::operator*= (int n) {
+    if (this->getRows() > 0 && this->getCols() > 0) {
+        for (int i = 0; i < this->getRows(); ++i) {
+            for (int j = 0; j < this->getCols(); ++j) {
+                this->m[i][j] += this->getZeroBased(i, j);
+            }
+        }
+    }
+}
+
+void Matrix::operator*= (Matrix& A) {
+    if (A.getRows() == this->getCols()) {
+        Matrix result(this->getRows(), A.getCols());
+        for (int i = 0; i < this->getRows(); ++i) {
+            for (int j = 0; j < A.getCols(); ++j) {
+                for (int k = 0; k < this->getCols(); ++k) {
+                    result.setZeroBased(i, j, result.getZeroBased(i, j) + this->getZeroBased(i, k) * A.getZeroBased(k, j));
+                }
+            }
+        }
+        *(this) = result;
+    }
+}
+
+Matrix Matrix::operator~ () const {
+    Matrix transposed(this->getCols(), this->getRows());
+    for (int i = 0; i < this->getRows(); ++i) {
+        for (int j = 0; j < this->getCols(); ++j) {
+            transposed.setZeroBased(i, j, this->getZeroBased(j, i));
+        }
+    }
+    return transposed;
+}
+
+std::ostream &operator<< (std::ostream &output, const Matrix &A) {
+    for (int i = 0; i < A.getRows(); ++i) {
+        for (int j = 0; j < A.getCols(); ++j) {
+            output << A.getZeroBased(i, j) << " ";
+        }
+        output << std::endl;
+    }
+    return output;
+}
+
+std::istream &operator>> (std::istream &input, Matrix &A) {
+    double value = 0;
+
+    for (int i = 0; i < A.getRows(); ++i) {
+        for (int j = 0; j < A.getCols(); ++j) {
+            input >> value;
+            A.setZeroBased(i, j, value);
+        }
+    }
+    return input;
 }
--- atv_5/Parte 2/Matrix.hpp
+++ atv_5/Parte 2/Matrix.hpp
@@ -1,45 +1,53 @@
-#include <iostream>
+#ifndef __MATRIX_H
+#define __MATRIX_H
 
-using namespace std;
+#include <fstream>
 
 class Matrix {
-private:
-  double **m; // m é um array 2D a ser implementado como um pointer de pointers
-  int nRows;  // numero de linhas
-  int nCols;  //  numero de colunas
+    private:
 
-public:
-  Matrix(int rows = 0, int cols = 0, const double &value = 0.0);
-  Matrix(const Matrix &M);
-  Matrix(ifstream &myFile);
-  ~Matrix();
+        double** m; // m é um array 2D a ser implementado como um pointer de pointers
+        int nRows;  // numero de linhas
+        int nCols;  //  numero de colunas
 
-  // basic getters
-  inline int getRows() const { return this->nRows; };
-  inline int getCols() const { return this->nCols; };
-  double get(int row, int col) const;
-  double getZeroBased(int row, int col) const;
+    public:
 
-  // setters
-  void setValue(int row, int column, double value);
+        // Construtores
+        Matrix(int rows = 0, int cols = 0, const double &value = 0.0);
+        Matrix(std::ifstream& myFile);
+        Matrix(const Matrix& that);
+        // destrutor
+        ~Matrix();
+    
+        // basic getters
+        inline int getRows() const { return this->nRows; }
+        inline int getCols() const { return this->nCols; }
+        double get(int row, int col) const;   
+        double getZeroBased(int row, int col) const;
+        
+        // overloaded operators
+        double& operator() (int i, int j);
+        Matrix operator+ (const Matrix& C) const;
+        Matrix operator* (const Matrix& A) const;;
+        Matrix operator- (const Matrix& A) const;
+        bool operator== (const Matrix& A) const;
+        bool operator!= (const Matrix& A) const;
+        void operator= (const Matrix& A);
+        void operator-= (const Matrix& A);
+        void operator+= (const Matrix& A);
+        void operator*= (int n);
+        void operator*= (Matrix &A);
+        Matrix operator~ () const;
+        void setZeroBased(int row, int col, double value);
+        friend std::ostream &operator<< (std::ostream &output, const Matrix &A); 
+        friend std::istream &operator>> (std::istream &input, Matrix &A); 
 
-  void print() const;
-
-  // Operadores
-  double &operator()(int i, int j) const;
-
-  Matrix &operator+(const Matrix &M) const;
-  Matrix &operator+=(const Matrix &M);
-  Matrix &operator-(const Matrix &M) const;
-  Matrix &operator-=(const Matrix &M);
-  Matrix operator*(const Matrix &M) const;
-  Matrix &operator*=(const Matrix &M);
-  Matrix &operator*=(int value);
-  Matrix operator=(const Matrix &M);
-  Matrix &operator~() const;
-  bool operator==(const Matrix &M) const;
-  bool operator!=(const Matrix &M) const;
-
-  friend istream &operator>>(istream &op, Matrix &M);
-  friend ostream &operator<<(std::ostream &output, const Matrix &A);
+        // other methods
+        void print() const;
+        void unit();
+        void zeros();
+        void ones();
+           
 };
+
+#endif
