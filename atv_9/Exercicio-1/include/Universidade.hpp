63ace38b0080b1f1712c76e89377380e5519f9e4
Luiz Almeida <luizorst@hotmail.com>
2021-02-25 18:15:17 -0300

    [IMPROVEMENT] change to smart pointers

--- atv_9/Exercicio-1/include/Universidade.hpp
+++ atv_9/Exercicio-1/include/Universidade.hpp
@@ -1,11 +1,12 @@
 #include "Professor.hpp"
 #include <vector>
+#include <memory>
 
 using std::vector;
 
 class Universidade {
 private:
-    vector<Professor *> professores;
+    vector<std::shared_ptr<Professor>> professores;
     double saldo;
 
 public:
@@ -13,7 +14,7 @@ public:
     ~Universidade();
     void addProfessor(std::string n, double sm);
     void addProfessor(std::string n, double nht, double vh);
-    void addProfessor(Professor *t);
+    void addProfessor(std::shared_ptr<Professor> t);
     double totalPago() const;
     void listaProf_Salario() const;
 };
--- atv_9/Exercicio-1/src/Universidade.cpp
+++ atv_9/Exercicio-1/src/Universidade.cpp
@@ -2,23 +2,24 @@
 #include "../include/ProfHorista.hpp"
 #include "../include/ProfIntegral.hpp"
 #include <iostream>
+#include <memory>
 
 Universidade::Universidade(double s) : saldo(s){};
 
 Universidade::~Universidade() {
-    for (int i = 0; i < professores.size(); ++i) {
-        Professor *temp = professores[i];
-        delete temp;
-    }
 }
 
 void Universidade::addProfessor(std::string n, double sm) {
-    this->professores.push_back(new ProfIntegral(n, sm));
+    this->professores.push_back(std::shared_ptr<ProfIntegral>(new ProfIntegral(n, sm)));
 }
 
 void Universidade::addProfessor(std::string n, double nht, double vh) {
-    this->professores.push_back(new ProfHorista(n, nht, vh));
+    this->professores.push_back(std::shared_ptr<ProfHorista>(new ProfHorista(n, nht, vh)));
 }
+
+void Universidade::addProfessor(std::shared_ptr<Professor> t) {
+    this->professores.push_back(t);
+};
 
 double Universidade::totalPago() const {
     double totalPago = 0;
@@ -28,10 +29,12 @@ double Universidade::totalPago() const {
 }
 
 void Universidade::listaProf_Salario() const {
-    std::cout << "*-*-*-*-*-*-*-*-*-*-*-*-*-*\n" << std::endl
+    std::cout << "*-*-*-*-*-*-*-*-*-*-*-*-*-*\n"
+              << std::endl
               << "LISTA PROF - SALARIO\n\n";
     for (auto &i : professores) {
         std::cout << i->getName() << "   " << i->getSalario() << std::endl;
     }
-    std::cout << "\n*-*-*-*-*-*-*-*-*-*-*-*-*-*\n" << std::endl;
+    std::cout << "\n*-*-*-*-*-*-*-*-*-*-*-*-*-*\n"
+              << std::endl;
 }
